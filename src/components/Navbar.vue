9c60b0167f3000730243731b21beeff6d5e5ea07
ryowuandjanet <ryowudata@gmail.com>
2021-02-26 17:51:01 +0800

    initial

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav>
+    <v-toolbar
+      app
+      flat
+    >
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+    <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+ 
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="primary"
+    >
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      
+    </v-navigation-drawer>
+ 
+  </nav>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Projects', route: '/projects' },
+        { icon: 'person', text: 'Team', route: '/team' },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Deshboard</h1>
+      <v-container class="my-5">
+        <p>
+          蛋是生命的來源，是活力早餐不可或缺的必需品。一顆蛋含有蛋白質、維生素和礦物質等營養素，是營養價值高的最強食材。
+        </p>
+        <p>
+          高麗菜含有維生素C和膳食纖維；起司則含有豐富的鈣。用這些愛心食材把蛋團團包圍，吃起來的味道也會變得有所不同。周圍食材絕妙地控制了蛋的受熱程度，讓它達到最完美的濃稠狀態。
+        </p>
+      </v-container>
+  </div>
+</template>
